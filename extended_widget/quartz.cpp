eb771ecac256421ff8b90349ae7d35dbf5564011
Zeta <zeta@zeta2374.com>
2023-05-03 19:38:50 +0800

    quartz calculator OK

--- extended_widget/quartz.cpp
+++ extended_widget/quartz.cpp
@@ -14,8 +14,8 @@ struct Q : z::AsciiWindow
 			|    |   |   |   |   |   |   |   |   |   | 
 			|    
 			|    B>-----B?-----B@-----Z:-----
-			|    |AC|   |%|    ||     ||
-			|    |      |      |      |
+			|    |AC|   |%|    ||     ||    
+			|    |      |      |      |    
 			|    |      |      |      |
 			|    B7-----B8-----B9-----B:-----
 			|    |7|    |8|    |9|    |-|
@@ -41,15 +41,15 @@ struct Q : z::AsciiWindow
 		{ q[i].resize(*Z[i]);
 			*this + q[i];
 		}
-		bt.resize(*Z[10]);
 		*this + bt;
-		*this << bt;
+		bt.resize(*Z[10]); // resize just change widget's matrix dimension
+		bt.draw(); // need to redraw with resized matrix
 		B[16]->text("\u00f7");
+		B[10]->text("\u2212");
 		organize_accordingto_zindex();
 		start();
 		for(int i=0; i<10; i++) B[i]->click([this, i]() { enter(i);});
-		B[10]->text("\u2212");
-		B[10]->click([this]() { // - press
+		B[10]->click([this]() { // -
 			store = current;
 			mode = '-';
 			current = 0;
@@ -60,19 +60,30 @@ struct Q : z::AsciiWindow
 			current = 0;
 		});
 		B[12]->click([this]() { // = press
-			if(mode == '-') current = store - current;
-			else if(mode == '+') current += store;
-			else if(mode == '*') current *= store;
-			else return;
+			switch(mode) {
+				case '-': current = store - current; break;
+				case '+': current += store; break;
+				case '*': current *= store; break;
+				case '/': current = store / current; break;
+				default: return;
+			}
 			set(current);
 		});
-		B[14]->click([this]() {
+		B[14]->click([this]() {// AC
 			current = 0;
 			mode = ' ';
 			set(0);
 		});
-
-		bt.click([this]() {
+		B[15]->click([this]() {
+			current /= 100;
+			set(current);
+		});
+		B[16]->click([this]() {// /
+			store = current;
+			mode = '/';
+			current = 0;
+		});
+		bt.click([this]() {// *
 			store = current;
 			mode = '*';
 			current = 0;
@@ -83,7 +94,7 @@ struct Q : z::AsciiWindow
 	z::Button bt{"\u00d7", {0,0,1,1}};
 	long current = 0, store;;
 	char mode = ' ';
-	void enter(long i) {
+	void enter(int i) {
 		if(q[1].number() == 10) {
 			current *= 10;
 			current += i;
