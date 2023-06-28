6472b1711188ecae230aeb9b215e7e0db274c5d2
Zeta <zeta@zeta2374.com>
2023-06-28 17:50:22 +0800

    canvas OK

--- src/canvas.cc
+++ src/canvas.cc
@@ -4,12 +4,15 @@ using namespace std;
 
 z::Canvas::Canvas(cv::Rect2i r) : z::Widget(r)
 {
+	static cv::Point2i before{-1, -1};
 	mat_ = cv::Vec3b{255, 255, 255};
-	user_callback_[cv::EVENT_LBUTTONDOWN] = [this](int xpos, int ypos){pen_down_ = true; cout << xpos -x << ' ' << ypos-y << endl;};
-	user_callback_[cv::EVENT_LBUTTONUP] = [this](int, int){pen_down_ = false;};
-	gui_callback_[cv::EVENT_MOUSEMOVE] = [this](int xpos, int ypos){
-		if(pen_down_) {
-			mat_[ypos-y][xpos-x] = draw_color_;
+	user_callback_[cv::EVENT_LBUTTONDOWN] = [this](int, int){pen_down_ = true;};
+	user_callback_[cv::EVENT_LBUTTONUP] = [this](int, int){pen_down_ = false; before.x = -1;};
+	gui_callback_[cv::EVENT_MOUSEMOVE] = [this](int xpos, int ypos)
+	{ if(pen_down_) 
+		{ cv::Point2i p{xpos - x, ypos - y};
+			if(before.x != -1) cv::line(mat_, before, p, draw_color_, 2);
+			before = p;
 			update();
 		}
 	};
